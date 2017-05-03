4312b88de454a9d4066a946c82a332672058dd5a
Sergei Zabolotskikh <serggzz@gmail.com>
2017-05-03 16:59:30 +0200

    add heap snapshot writer

--- include/AdblockPlus/JsEngine.h
+++ include/AdblockPlus/JsEngine.h
@@ -95,6 +95,8 @@ namespace AdblockPlus
     };
     typedef std::list<JsWeakValuesList> JsWeakValuesLists;
   public:
+
+    void WriteHeapSnapshot(const std::string& fileName);
     /**
      * Event callback function.
      */
